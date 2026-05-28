a750f88f32fa3ac8c4aae54722fc5c0038bdb35c
camper3d <camper3d@gmail.com>
2026-05-28 18:06:11 +0300

    feat: добавлены настройки приложения через пайдентик для постгреса и рэббита

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    database_url: str = "postgresql+asyncpg://payment_user:payment_pass@localhost:5432/payment_db"
+
+    rabbitmq_url: str = "amqp://payment_user:payment_pass@localhost:5672/"
+
+    api_key: str = "test-api-key-123"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
